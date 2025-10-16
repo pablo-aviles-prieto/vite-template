b78fbc851de6c62b17d78f071b5e5da38c30b043
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2025-10-16 23:05:05 +0200

    fix(workflow): modify biome workflow to install dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { App } from '@/App';
+import { App } from '@/app';
+import './styles/index.css';
 
 // biome-ignore lint/style/noNonNullAssertion: <implemented by vite>
 createRoot(document.getElementById('root')!).render(
